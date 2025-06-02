a0909cfd2194409957ce63e15d71a7613f928c4a
ng9586 <ngk85588@gmail.com>
2025-06-02 11:28:05 +0000

    First update

---
+++ travel-agency-frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Hotels from './pages/Hotels';
+import Dashboard from './pages/Dashboard';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Hotels />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
